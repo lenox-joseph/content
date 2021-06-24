7dbdc8e5ff177da19aa12a2fe133eece1dc7f24a
Lenox, Joseph D (Collins) <Joseph.Lenox@collins.com>
2021-06-23 20:06:51 -0500

    Add initial scan and remediation for setting shadow account passwords min/max lifespans. Signed-off-by: Lenox, Joseph D (Collins) <Joseph.Lenox@collins.com>

---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
@@ -0,0 +1,15 @@
+# platform = Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel
+. /usr/share/scap-security-guide/remediation_functions
+{{{ bash_instantiate_variables("var_accounts_maximum_age_login_defs") }}}
+
+# only remediate interactive user accounts.
+for acct in $(awk -F: '$3 >= 1000 && 
+                       $3 != 65534 && 
+                       $3 != 65535 &&
+                       $3 != 4294967294 && 
+                       $3 != 4294967295 { print $1 }' /etc/passwd); do
+    # only remediate accounts that are actually failed.
+    if [ $(chage -l $acct | awk -F: '/Maximum/ {print $2}') -lt $var_accounts_maximum_age_login_defs ]; then
+        chage -M $var_accounts_maximum_age_login_defs $acct
+    fi
+done
